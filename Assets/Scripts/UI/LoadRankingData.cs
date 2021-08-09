cbad33ba5e8d81b0018da29710e9a7bf1525b566
Wagner GFX <greyfox_esb@hotmail.com>
2021-08-09 18:54:15 -0300

    Changed InitializeSaveData to LoadRankingData
    To better explain the class functionality.
    Also removed an unused Ui element in the Ranking scene.

--- Assets/Scripts/UI/LoadRankingData.cs
+++ Assets/Scripts/UI/LoadRankingData.cs
@@ -3,7 +3,7 @@ using UnityEngine.UI;
 
 namespace TowerDungeon.UI
 {
-    public class InitializeSaveData : MonoBehaviour
+    public class LoadRankingData : MonoBehaviour
     {
         [SerializeField]
         private Text pointsTxt, coinsTxt;
